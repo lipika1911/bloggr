84d74883560747343aeb36dc4d8b0b2d78bdaf5c
lipika1911 <lipikaarya1723@gmail.com>
2025-07-21 16:00:21 +0530

    feat: add blog cards and newsletter component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import {assets} from '../assets/assets'
 import Navbar from '../components/Navbar'
 import Header from '../components/Header'
+import BlogList from '../components/BlogList'
+import Newsletter from '../components/Newsletter'
 
 const Home = () => {
   return (
     <>
         <Navbar />
         <Header />
+        <BlogList />
+        <Newsletter />
     </>
   )
 }
