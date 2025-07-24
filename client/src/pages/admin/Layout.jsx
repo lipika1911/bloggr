d5afcd0adf1bfd747c084d74c965fb5788e69848
lipika1911 <lipikaarya1723@gmail.com>
2025-07-24 20:08:23 +0530

    fix: clear auth token and state on user logout

--- client/src/pages/admin/Layout.jsx
+++ client/src/pages/admin/Layout.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
-import { Outlet, useNavigate } from 'react-router-dom'
+import { Outlet} from 'react-router-dom'
 import { assets } from '../../assets/assets';
 import Sidebar from '../../components/admin/Sidebar';
+import { useAppContext } from '../../context/AppContext';
+import toast from 'react-hot-toast';
 
 const Layout = () => {
-  const navigate = useNavigate();
+  const {axios, setToken, navigate} = useAppContext();
 
   const logout = () => {
+    localStorage.removeItem('token');
+    axios.defaults.headers.common['Authorization'] = null;
+    setToken(null);
     navigate('/')
   }
 
